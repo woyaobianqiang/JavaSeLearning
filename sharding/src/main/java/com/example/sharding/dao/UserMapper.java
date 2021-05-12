cf140d37b9e7f37e98ce5922a4dce6b1f06df700
zhanshengyou <1038039989@qq.com>
2021-05-12 14:05:44 +0800

    sharding分表demo

---
+++ sharding/src/main/java/com/example/sharding/dao/UserMapper.java
@@ -0,0 +1,13 @@
+package com.example.sharding.dao;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.sharding.entity.User;
+
+/**
+ * user dao层
+ * @author lihaodong
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
